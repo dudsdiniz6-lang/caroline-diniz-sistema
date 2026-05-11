a5a18714ade9369372e4c102d836767a697c5dbe
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-05-11 10:04:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function abrirModal() {
 function fecharModal() {
   document.getElementById("modal").style.display = "none";
 }
+function salvarAgendamento() {
+
+  const cliente = document.getElementById("cliente").value;
+  const horario = document.getElementById("horario").value;
+
+  const coluna = document.querySelector(".column");
+
+  const card = document.createElement("div");
+
+  card.classList.add("appointment");
+
+  card.innerHTML = `
+    <strong>${cliente}</strong>
+    <span>Novo Atendimento</span>
+    <small>${horario}</small>
+  `;
+
+  coluna.appendChild(card);
+
+  fecharModal();
+}
