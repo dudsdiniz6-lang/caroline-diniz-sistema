b6b2b9ac586115cf1f66f7cdcaaeab21022b8916
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-05-12 14:39:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,54 @@ card.style.top = `${posicaoTop}px`;
   `;
 
   coluna.appendChild(card);
+  const agendamento = {
+  cliente,
+  horario,
+  profissional
+};
+
+let agendamentos = JSON.parse(localStorage.getItem("agendamentos")) || [];
+
+agendamentos.push(agendamento);
+
+localStorage.setItem("agendamentos", JSON.stringify(agendamentos));
 
   fecharModal();
+}
+window.onload = function(){
+
+  const agendamentos = JSON.parse(localStorage.getItem("agendamentos")) || [];
+
+  agendamentos.forEach((agendamento)=>{
+
+    const colunas = document.querySelectorAll(".column");
+
+    const coluna = colunas[agendamento.profissional];
+
+    const card = document.createElement("div");
+
+    card.classList.add("appointment");
+
+    const [hora, minuto] = agendamento.horario.split(":");
+
+    const minutosTotais = (parseInt(hora) * 60) + parseInt(minuto);
+
+    const inicioAgenda = 14 * 60;
+
+    const diferenca = minutosTotais - inicioAgenda;
+
+    const posicaoTop = (diferenca / 20) * 80;
+
+    card.style.top = `${posicaoTop}px`;
+
+    card.innerHTML = `
+      <strong>${agendamento.cliente}</strong>
+      <span>Novo Atendimento</span>
+      <small>${agendamento.horario}</small>
+    `;
+
+    coluna.appendChild(card);
+
+  });
+
 }
