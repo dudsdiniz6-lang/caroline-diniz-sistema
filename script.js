2ed5e718ce906a719262fc60e27304d7ebb89a35
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-05-20 18:19:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ const SUPABASE_KEY = "sb_publishable_KQ58nMCXUZl0Nz5jEHkKKg_RbpL-QTw";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let dataSelecionada = new Date();
-
+const horariosAgenda = [
+  "08:00",
+  "08:30",
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00"
+];
 const usuarios = [
   {
     usuario:"eduarda",
