6e22e77495dcc295f1c83f7d55bf246cb6834d4b
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-05-21 18:47:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2256,61 +2256,19 @@ function atualizarServicoSelecionado(){
   }
 }
 
-  function abrirModalServico(){
+ function abrirModalServico(){
 
   servicoEditandoId = null;
 
-  document.getElementById("nomeServicoSala
-function carregarCategoriasServicos(){
+  document.getElementById("nomeServicoSalao").value = "";
+  document.getElementById("categoriaServicoSalao").value = "";
+  document.getElementById("descricaoServicoSalao").value = "";
+  document.getElementById("comissaoServicoSalao").value = "";
+  document.getElementById("duracaoServicoSalao").value = "";
+  document.getElementById("valorServicoSalao").value = "";
+  document.getElementById("custoServicoSalao").value = "";
 
-  const select = document.getElementById("categoriaServicoSalao");
-  const chips = document.getElementById("categorias-servicos-chips");
-
-  if(select){
-    select.innerHTML = `<option value="">Selecione a categoria</option>`;
-  }
-
-  if(chips){
-    chips.innerHTML = "";
-  }
-
-  supabaseClient
-    .from("categorias_servicos")
-    .select("*")
-    .then((resposta)=>{
-
-      const categorias = resposta.data || [];
-
-      categorias.forEach((categoria)=>{
-
-        if(select){
-          select.innerHTML += `
-            <option value="${categoria.nome}">
-              ${categoria.nome}
-            </option>
-          `;
-        }
-
-        if(chips){
-          chips.innerHTML += `
-            <div class="chip-categoria-box">
-              <button class="chip-categoria">
-                ${categoria.nome}
-              </button>
-
-              <button
-                class="btn-excluir-categoria"
-                onclick="excluirCategoriaServico('${categoria.id}')"
-              >
-                ×
-              </button>
-            </div>
-          `;
-        }
-
-      });
-
-    });
+  document.getElementById("modal-servico").style.display = "flex";
 
 }
 function adicionarCategoriaServico(){
