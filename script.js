b3cd9689b5f9817e5c104aa239a04a0f5d9d0c55
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-05-30 12:56:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3912,38 +3912,7 @@ async function carregarProfissionaisAgenda(){
   }
 
   carregarAgenda();
-   if(agendaBody){
-
-  const colunaTempo =
-    agendaBody.querySelector(".time-column");
-
-  if(colunaTempo){
-
-    colunaTempo.innerHTML =
-      horariosAgenda
-        .map(horario => `
-
-          <div style="
-            height:80px;
-            display:flex;
-            align-items:flex-start;
-            justify-content:center;
-            padding-top:6px;
-            box-sizing:border-box;
-            border-bottom:1px solid #eee;
-            font-size:12px;
-            color:#666;
-            font-weight:600;
-          ">
-            ${horario}
-          </div>
-
-        `)
-        .join("");
-
-  }
-
-}
+  
 function carregarProfissionais(){
 
   const lista = document.getElementById("lista-profissionais");
