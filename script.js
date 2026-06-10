b086d4b5eebcb0a2dfda0ccb93184ea2e14a2750
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-06-10 16:07:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -735,8 +735,10 @@ grade.innerHTML = `
               <div class="agenda-coluna-corpo" style="height:${alturaAgenda}px;">
 
             ${horarios.map(h=>`
+ ${horarios.map(h=>`
   <div
     class="agenda-slot"
+    data-horario="${h}"
     onclick="abrirOpcoesHorarioAgenda('${profissional.id}', '${h}')"
   ></div>
 `).join("")}
