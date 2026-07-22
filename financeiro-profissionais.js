e39b9fdba3e92f1a11271cae480b90d97adf7b8b
dudsdiniz6-lang <dudsdiniz6@gmail.com>
2026-07-22 09:33:45 -0300

    Update financeiro-profissionais.js

--- financeiro-profissionais.js
+++ financeiro-profissionais.js
@@ -1,1 +1,95 @@
+"use strict";
 
+window.FinanceiroProfissionais = {
+
+    iniciar,
+
+    abrirAba,
+
+    resumo: {
+        carregar: carregarResumo
+    },
+
+    pagamentos: {
+        carregar: carregarPagamentos
+    },
+
+    vales: {
+        carregar: carregarVales
+    },
+
+    extrato: {
+        carregar: carregarExtrato
+    },
+
+    historico: {
+        carregar: carregarHistorico
+    }
+
+};
+
+function iniciar(){
+
+    console.log("Financeiro dos profissionais carregado.");
+
+}
+
+function abrirAba(nome){
+
+    switch(nome){
+
+        case "resumo":
+            FinanceiroProfissionais.resumo.carregar();
+            break;
+
+        case "pagamentos":
+            FinanceiroProfissionais.pagamentos.carregar();
+            break;
+
+        case "vales":
+            FinanceiroProfissionais.vales.carregar();
+            break;
+
+        case "extrato":
+            FinanceiroProfissionais.extrato.carregar();
+            break;
+
+        case "historico":
+            FinanceiroProfissionais.historico.carregar();
+            break;
+
+    }
+
+}
+
+async function carregarResumo(){
+
+    console.log("Resumo");
+
+}
+
+async function carregarPagamentos(){
+
+    console.log("Pagamentos");
+
+}
+
+async function carregarVales(){
+
+    console.log("Vales");
+
+}
+
+async function carregarExtrato(){
+
+    console.log("Extrato");
+
+}
+
+async function carregarHistorico(){
+
+    console.log("Histórico");
+
+}
+
+document.addEventListener("DOMContentLoaded", iniciar);
